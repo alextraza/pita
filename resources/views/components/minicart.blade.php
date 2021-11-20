2bc9d05cb60fac38b702932fb85c5d68d1a4d443
teiuqpeek <gurdin.k@gmail.com>
2021-11-20 17:59:34 +0300

    cart button change :after to span

--- resources/views/components/minicart.blade.php
+++ resources/views/components/minicart.blade.php
@@ -1,11 +1,12 @@
 <a href="{{ route('cart.index') }}">
     <div class="cart__name">Корзина</div>
-    <div class="cart__icon" data-num="6">
+    <div class="cart__icon" >
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="">
             <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>
             <line x1="3" y1="6" x2="21" y2="6"></line>
             <path d="M16 10a4 4 0 0 1-8 0"></path>
         </svg>
+        <span data-num="6"></span>
     </div>
 </a>

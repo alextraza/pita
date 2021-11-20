6619b976819ec2b86ce1c312a8241ef5b14d1a34
teiuqpeek <gurdin.k@gmail.com>
2021-11-20 18:40:26 +0300

    button cart

--- resources/views/components/minicart.blade.php
+++ resources/views/components/minicart.blade.php
@@ -1,6 +1,7 @@
 <a href="{{ route('cart.index') }}">
-    <div class="cart__icon" >
+  
         <div class="cart__name">@if (Cart::total()){{ Cart::total() }} @else Корзина @endif</div>
+        <div class="cart__icon" >
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="">
             <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>

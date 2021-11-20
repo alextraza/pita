e84d3b14b887d6f50b1d3d206ff322a7a709fd22
alextraza <a1extra3a@gmail.com>
2021-11-21 01:28:46 +0200

    add user menu

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -16,11 +16,33 @@
             <li class="menu__item"><a href="{{ route('page', ['slug' => 'about']) }}">О нас</a></li>
             <li class="menu__item"><a href="{{ route('page', ['slug' => 'delivery']) }}">Доставка и оплата</a></li>
             <li class="menu__item"><a href="{{ route('page', ['slug' => 'contacts'])  }}">Контакты</a></li>
-            <li class="menu__item"> <a href="#">Вход<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></a></li>
+            <li class="menu__item">
+                <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></a>
+                <ul>
+                    @guest
+                    <li>
+                        <a href="/login">Войти</a>
+                    </li>
+                    <li>
+                        <a href="/register">Регистрироваться</a>
+                    </li>
+                    @endguest
+                    @auth
+                    <li>
+                        <a href="/user">Кабинет</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('signout') }}">Выйти</a>
+                    </li>
+                    @endauth
+                </ul>
+            </li>
         </ul>
     </div>
     <div class="user__log">
-        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></a>
+        <a href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+        </a>
     </div>
     
 </header>

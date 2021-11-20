621a03eddb450d6fee10adb4af0316e28d8fb1c7
alextraza <a1extra3a@gmail.com>
2021-11-20 16:53:48 +0200

    add code to minimap

--- resources/views/components/minicart.blade.php
+++ resources/views/components/minicart.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ route('cart.index') }}">
-    <div class="cart__name">Корзина</div>
-    <div class="cart__icon" data-num="6">
+    <div class="cart__name">@if (Cart::total()){{ Cart::total() }} @else Корзина @endif</div>
+    <div class="cart__icon"@if (Cart::count()) data-num="{{ Cart::count() }}" @endif>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="">
             <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>

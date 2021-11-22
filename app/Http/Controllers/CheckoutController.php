baf44371d32135bc4d9358c30befd5cf7386b65f
alextraza <a1extra3a@gmail.com>
2021-11-22 10:24:13 +0200

    add new request & store action

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Cart;
+use App\Models\{Order, Address, User};
+use App\Http\Requests\StoreOrder;
 
 class CheckoutController extends Controller
 {
+    /**
+    * Show checkout page
+    *
+    * @return view
+    */
     public function index()
     {
         return view('checkout');
+    }
+
+    /**
+    * Store order from checkout
+    *
+    * @param App\Http\Request\StoreOrder $request
+    */
+    public function srore(StoreOrder $request)
+    {
+        //
     }
 }

f6966cb57dd734aafd6045eca7fa8246deb1d535
alextraza <a1extra3a@gmail.com>
2021-11-24 01:05:27 +0200

    add messages modal

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -7,3 +7,13 @@
         <a href="tel:{{ str_replace([' ', '(', ')', '-'], '', \App\Models\Config::phone()) }}">{{ \App\Models\Config::phone() }}</a>
     </div>
 </footer>
+<div id="dialog" class="modal @if (Session::get('success')) active @endif">
+    <div class="modal__overlay">
+    </div>
+    <div class="modal__window">
+        <div class="modal__close">&times;</div>
+        <div class="modal__inner">
+            @if (Session::get('success')) {{ Session::get('success') }} @endif
+        </div>
+    </div>
+</div>

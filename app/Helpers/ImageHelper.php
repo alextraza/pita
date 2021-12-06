ec1eaca7d21d9418f1c0946f18e9b6d14d8f00ef
alextraza <a1extra3a@gmail.com>
2021-12-06 13:29:49 +0200

    add extention to ImageHelper

--- app/Helpers/ImageHelper.php
+++ app/Helpers/ImageHelper.php
@@ -18,14 +18,14 @@ class ImageHelper
         return false;
     }
 
-    private static function getCachePath($filename, $width, $height)
+    private static function getCachePath($filename, $width, $height, $ext)
     {
         if ($path = static::getImagePath($filename)) {
             $pathInfo = pathinfo($path);
             if ($pathInfo['extension'] == 'svg') {
                 return asset(static::$storePath . $filename);
             }
-            $filename = $pathInfo['filename'] . '-' . $width . '-' . $height . '.webp';
+            $filename = $pathInfo['filename'] . '-' . $width . '-' . $height . '.' . $ext;
         }
         $path = public_path(static::$storePath . $filename);
         if (file_exists($path)) {
@@ -34,21 +34,21 @@ class ImageHelper
         return false;
     }
 
-    public static function thumb($filename, $width, $height)
+    public static function thumb($filename, $width, $height, $ext='webp')
     {
         if (!$filename) {
             return null;
         }
 
-        if ($image = static::getCachePath($filename, $width, $height)) {
+        if ($image = static::getCachePath($filename, $width, $height, $ext)) {
             return $image;
         }
 
         if ($originPath = static::getImagePath($filename)) {
             $pathInfo = pathinfo($originPath);
-            $name = $pathInfo['filename'] . '-' . $width . '-' .$height. '.webp';
+            $name = $pathInfo['filename'] . '-' . $width . '-' .$height. '.' . $ext;
             $path = public_path(static::$storePath . $name);
-            $image = Image::make($originPath)->encode('webp', 90);
+            $image = Image::make($originPath)->encode($ext, 90);
             $image->resize($width, $height, function($const) {
                 $const->aspectRatio();
             })->save($path);

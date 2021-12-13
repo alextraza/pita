546704dd9506d1d194a4517a0d940de4782ccc4f
alextraza <a1extra3a@gmail.com>
2021-12-13 15:32:00 +0200

    some fixes

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -137,8 +137,6 @@ class CheckoutController extends Controller
                     $order->save();
                 }
             }
-            return redirect(route('index'))->withSuccess('Оплата принята. Спасибо за заказ!');
         }
-        return redirect(route('index'))->withSuccess('Произошла ошибка при оплате!');
     }
 }

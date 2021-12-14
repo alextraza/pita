0e80879ca3442a1785c0dc8e7e5657dda34d8d98
teiuqpeek <gurdin.k@gmail.com>
2021-12-14 18:40:53 +0300

    Update bug.blade.php

--- resources/views/components/bug.blade.php
+++ resources/views/components/bug.blade.php
@@ -1,46 +1,55 @@
 @if ($gtm = \App\Models\Config::gtm())
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id={!! $gtm !!}"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id={!! $gtm !!}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-  gtag('config', '{!! $gtm !!}');
-</script>
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
 
-<!-- Google Analytics -->
-<script async>
-    (function(i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function() {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+        gtag('config', '{!! $gtm !!}');
+    </script>
 
-    ga('create', '{!! $gtm !!}', 'auto');
-    ga('send', 'pageview');
-</script>
+    <!-- Google Analytics -->
+    <script async>
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', '{!! $gtm !!}', 'auto');
+        ga('send', 'pageview');
+    </script>
 @endif
 
 
 
-{{-- @if ($yam = \App\Models\Config::yam())  
+@if ($yam = \App\Models\Config::yam())  
  <script type=\"text/javascript\" >
-     (function (d, w, c) {
-         (w[c] = w[c] || []).push(function() {
-             try {
+    (function(m, e, t, r, i, k, a) {
+            m[i] = m[i] || function() {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(
+                k, a)
+        })
                  w.yaCounter{!! $yam !!} = new Ya.Metrika({
                      id:{!! $yam !!},
                      clickmap:true,
-                     trackLinks:true,
-                     accurateTrackBounce:true,
-                     webvisor:true
+         trackLinks:true,
+         accurateTrackBounce:true,
+         webvisor:true,
+         ecommerce:"dataLayer"
                  });
              } catch(e) { }
          });
@@ -57,24 +66,8 @@
          } else { f(); }
      })(document, window, \"yandex_metrika_callbacks\");
  </script>
- <noscript><div><img src=\"https://mc.yandex.ru/watch/"{!! $yam !!}"\" style=\"position:absolute; left:-9999px;\" alt=\"\" /></div></noscript> 
- @endif --}}
+ <noscript><div><img src=\"https://mc.yandex.ru/watch/{!! $yam !!}\" style=\"position:absolute; left:-9999px;\" alt=\"\" /></div></noscript> 
+ @endif 
 
- 
- @if ($yam = \App\Models\Config::yam()) 
- <!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
- 
-    ym(86746920, "init", {
-         clickmap:true,
-         trackLinks:true,
-         accurateTrackBounce:true,
-         webvisor:true,
-         ecommerce:"dataLayer"
-    });
- </script>
-  <noscript><div><img src=\"https://mc.yandex.ru/watch/"{!! $yam !!}"\" style=\"position:absolute; left:-9999px;\" alt=\"\" /></div></noscript>
- <!-- /Yandex.Metrika counter --> @endif
+
+

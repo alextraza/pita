4aca40bcdf144afeb2bf68cf84b046c996b48094
teiuqpeek <gurdin.k@gmail.com>
2021-12-14 18:19:39 +0300

    add styles

--- resources/views/components/bug.blade.php
+++ resources/views/components/bug.blade.php
@@ -30,7 +30,7 @@
 
 
 
-@if ($yam = \App\Models\Config::yam())  
+{{-- @if ($yam = \App\Models\Config::yam())  
  <script type=\"text/javascript\" >
      (function (d, w, c) {
          (w[c] = w[c] || []).push(function() {
@@ -58,6 +58,23 @@
      })(document, window, \"yandex_metrika_callbacks\");
  </script>
  <noscript><div><img src=\"https://mc.yandex.ru/watch/"{!! $yam !!}"\" style=\"position:absolute; left:-9999px;\" alt=\"\" /></div></noscript> 
- @endif
+ @endif --}}
 
  
+ @if ($yam = \App\Models\Config::yam()) 
+ <!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+    ym(86746920, "init", {
+         clickmap:true,
+         trackLinks:true,
+         accurateTrackBounce:true,
+         webvisor:true,
+         ecommerce:"dataLayer"
+    });
+ </script>
+  <noscript><div><img src=\"https://mc.yandex.ru/watch/"{!! $yam !!}"\" style=\"position:absolute; left:-9999px;\" alt=\"\" /></div></noscript>
+ <!-- /Yandex.Metrika counter --> @endif

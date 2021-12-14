72c84206fb7ff026684b00e5ca5481cfe3bbf08a
teiuqpeek <gurdin.k@gmail.com>
2021-12-14 18:41:22 +0300

    Update bug.blade.php

--- resources/views/components/bug.blade.php
+++ resources/views/components/bug.blade.php
@@ -34,7 +34,7 @@
 
 
 @if ($yam = \App\Models\Config::yam())  
- <script type=\"text/javascript\" >
+ <script async type=\"text/javascript\" >
     (function(m, e, t, r, i, k, a) {
             m[i] = m[i] || function() {
                 (m[i].a = m[i].a || []).push(arguments)

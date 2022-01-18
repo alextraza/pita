3997a2751d430305b0b0801bfd25aab1c6f8a9eb
teiuqpeek <gurdin.k@gmail.com>
2022-01-18 21:13:47 +0300

    metrika

--- resources/views/components/bug.blade.php
+++ resources/views/components/bug.blade.php
@@ -31,33 +31,23 @@
 
 
 @if ($yam = \App\Models\Config::yam())  
- <script async type=\"text/javascript\" >
-     (function (d, w, c) {
-         (w[c] = w[c] || []).push(function() {
-             try {
-                 w.yaCounter{!! $yam !!} = new Ya.Metrika({
-                     id:{!! $yam !!},
-                     clickmap:true,
-                     trackLinks:true,
-                     accurateTrackBounce:true,
-                     webvisor:true
-                 });
-             } catch(e) { }
-         });
 
-         var n = d.getElementsByTagName(\"script\")[0],
-             s = d.createElement(\"script\"),
-             f = function () { n.parentNode.insertBefore(s, n); };
-         s.type = \"text/javascript\";
-         s.async = true;
-         s.src = \"https://d31j93rd8oukbv.cloudfront.net/metrika/watch_ua.js\";
+<script async type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-         if (w.opera == \"[object Opera]\") {
-             d.addEventListener(\"DOMContentLoaded\", f, false);
-         } else { f(); }
-     })(document, window, \"yandex_metrika_callbacks\");
- </script>
- <noscript><div><img src=\"https://mc.yandex.ru/watch/{!! $yam !!}\" style=\"position:absolute; left:-9999px;\" alt=\"\" /></div></noscript> 
+   ym({!! $yam !!}, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true,
+        ecommerce:"dataLayer"
+   });
+</script>
+<noscript><div><img loading="lazy" src="https://mc.yandex.ru/watch/{!! $yam !!}" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+
+
  @endif
 
  
